f8696c18918ce231e832b2e2b2dd7cd31741e162
zawazawazawazawa <bosse.de.poulet@gmail.com>
2019-10-22 16:46:24 +0900

    delete comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # todo: 今月、翌月、翌々月に絞りたい
     start_date = Date.today
     end_date = Date.today.next_month.next_month.at_end_of_month
     @holidays = Holiday.where(date: start_date..end_date)
